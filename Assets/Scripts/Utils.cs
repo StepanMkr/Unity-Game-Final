06b7576d9dbd5ad86161efcd68047957841bedd0
StepanMkr <stepan.weightlifting@gmail.com>
2024-05-29 13:03:24 +0500

    Game Upload

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace KnightAdventure.Utils {
+
+    public static class Utils 
+    {
+        public static Vector3 GetRandomDir() => new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+    }
+}
